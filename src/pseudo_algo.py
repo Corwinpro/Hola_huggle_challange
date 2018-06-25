e86d244f0c0153d6702f56239095a95456981ae4
Petr Kungurtsev <petrkungurtsev@dhcp-10-248-133-251.eduroam.wireless.private.cam.ac.uk>
2018-06-25 11:42:26 +0100

    Added weights, bugs fixed

--- src/pseudo_algo.py
+++ src/pseudo_algo.py
@@ -13,11 +13,13 @@ class Agent():
         self.total = 0
         for i in range(len(self.counts)): 
             self.total += counts[i]*values[i]
-        self.p2_set = self.p2_values_set()
+        self.p2_set, self.p2_set_weights = self.p2_values_set()
         self.p2_offers = []
         self.my_offers = []
         self.threshold = 5
+        self.reweighting_param = 0.5
         self.Total_value = 10
+        #Optimal ofer J parameters
         self.acceptance_threshold = 8.
         self.diff_weight = 0.2
 
@@ -26,29 +28,40 @@ class Agent():
         for i in range(len(o)):
             profit += o[i]*value[i]
 
+    def average_of_set(self, my_set):
+        #For a set my_set, we calculated the weighted average of values
+        #Example: Value(hat) = Summ of value_i(hat)*probability(value_i) / total_probability
+        average_set = []
+        for i in range(len(self.values)):
+            _avg_value_i = 0.
+            for j in range(len(my_set)):
+                _avg_value_i += my_set[j][i]*self.p2_set_weights[j]
+            average_set.append(_avg_value_i/sum(self.p2_set_weights))
+        return average_set
+
     def p2_values_set(self):
         #Given a list of counts, we find all possible admissible values,
         #i.e. all lists V: (V,counts) == 10
-        return []
+        p2_set = [[0. for i in range(len(self.values))] for j in range(5)]
+        self.default_p2_set = self.average_of_set(p2_set)
+        #And assign initial weights for each set
+        #weight[i] == x: the values have x probability to appear in p2
+        p2_set_weights = [1. for i in range(len(self.values))]
+        return (p2_set, p2_set_weights)
 
     def update_p2_set(self, o):
         #Given an offer o, we update the p2_set list
-        #by removing all entities which would result in low opponents profit,
-        #i.e. if o.profit < self.threshold for v in p2_set, remove v
-        for value in self.p2_set:
+        #by reweighting all entities which would result in low opponents profit,
+        #i.e. if o.profit < self.threshold for v in p2_set, decrease probability(v)
+        for i in range(len(self.p2_set)):
+            value = self.p2_set[i]
             if self.offer_profit(o, value) < self.threshold:
-                self.p2_set.remove(value)
-        #If the p2_set is empty, we just use the average value for items
-        if len(self.p2_set == 0):
-            avg_value = [1.*self.Total_value/sum(self.counts)]
-            self.p2_set.append(avg_value*len(self.values))
+                self.p2_set_weights[i] *= self.reweighting_param
 
     def estimate_p2_profit(self, o):
-        #We estimate the average opponents profit by self.p2_set
-        p2_profit = 0
-        for value in self.p2_set:
-            p2_profit += self.offer_profit(o, value)
-        p2_profit /= len(self.p2_set)
+        #We estimate the expected opponents profit by average values of self.p2_set
+        average_p2_values = self.average_of_set(self.p2_set)
+        p2_profit = self.offer_profit(o, average_p2_values)
         return p2_profit
 
     def J_ac(self, p1_profit, p2_profit):
